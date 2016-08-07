0910913eb64f50beba5d4f7f7b244d45cab28d2b
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-08-07 14:53:09 -0500

    Basic format structs and classes + unit tests

---
+++ NanoPcap/Format.py
@@ -0,0 +1,250 @@
+
+# https://wiki.wireshark.org/Development/LibpcapFileFormat
+
+#TODO: support inverted byte order
+
+import datetime
+import struct
+import sys
+
+########## Constants ##########
+
+PCAP_MAGIC_NUMBER = 0xa1b2c3d4
+PCAP_NS_MAGIC_NUMBER = 0xa1b23c4d
+
+#Some special stuff for handling inverted byte order (not necessarily big endian -- these will be
+#used for little endian caps on a big endian system
+PCAP_MAGIC_NUMBER_INVERTED = 0xd4c3b2a1
+PCAP_NS_MAGIC_NUMBER_INVERTED = 0x4d3cb2a1
+
+MICROS_PER_SECOND = 1000 * 1000
+NANOS_PER_SECOND = MICROS_PER_SECOND * 1000
+
+PCAP_DEFAULT_TIME_RESOLUTION = NANOS_PER_SECOND
+
+########## Structs ##########
+
+#Unfortunately there is no direct way to tell the struct library to swap bytes, so we have to choose
+#Based on the magic value, the parser automatically switches between structs with or without this
+_structInvert = '>' if sys.byteorder == 'little' else '<'
+
+PCAP_HEADER_STRUCT_PATTERN = 'IHHiIII'
+PCAP_HEADER_STRUCT = struct.Struct(PCAP_HEADER_STRUCT_PATTERN)
+PCAP_HEADER_STRUCT_INVERTED = struct.Struct(_structInvert + PCAP_HEADER_STRUCT_PATTERN)
+
+PCAP_RECORD_HEADER_STRUCT_PATTERN = 'IIII'
+PCAP_RECORD_HEADER_STRUCT = struct.Struct(PCAP_RECORD_HEADER_STRUCT_PATTERN)
+PCAP_RECORD_HEADER_STRUCT_INVERTED = struct.Struct(_structInvert + PCAP_RECORD_HEADER_STRUCT_PATTERN)
+
+########## Types ##########
+
+class PcapHeader:
+    """
+    Represents a PCAP file header.
+    """
+
+    def __init__(self, magicNumber, versionMajor, versionMinor, tzOffset, sigfigs, snaplen, network):
+        if magicNumber < 0:
+            raise ValueError('magicNumber must not be negative')
+        if versionMajor < 0:
+            raise ValueError('versionMajor must not be negative')
+        if versionMinor < 0:
+            raise ValueError('versionMinor must not be negative')
+        if sigfigs < 0:
+            raise ValueError('sigfigs must not be negative')
+        if snaplen < 0:
+            raise ValueError('snaplen must not be negative')
+        if network < 0:
+            raise ValueError('network must not be negative')
+
+        self._magicNumber = magicNumber
+        self._timeResolution = 1000 * 1000 * 1000 if magicNumber == PCAP_NS_MAGIC_NUMBER else 1000 * 1000
+        self._versionMajor = versionMajor
+        self._versionMinor = versionMinor
+        self._tzOffset = tzOffset
+        self._sigfigs = sigfigs
+        self._snaplen = snaplen
+        self._network = network
+
+    def magicNumber(self):
+        """
+        Returns the magic number.
+
+        :return: int
+        """
+        return self._magicNumber
+
+    def isMagicValid(self):
+        """
+        Returns a bool indicating if the magic value is correct.
+
+        :return: bool
+        """
+        return self._magicNumber in [PCAP_MAGIC_NUMBER, PCAP_NS_MAGIC_NUMBER]
+
+    def timeResolution(self):
+        """
+        Returns the denominator of the fraction for which ts_frac is the numerator.
+        For files with microsecond resolution, this will be 1e6, and for those with
+        nanosecond resolution, it will be 1e9.
+
+        :return: int
+        """
+        return self._timeResolution
+
+    def versionMajor(self):
+        """
+        Returns the major version number.
+
+        :return: int
+        """
+        return self._versionMajor
+
+    def versionMinor(self):
+        """
+        Returns the minor version number.
+
+        :return: int
+        """
+        return self._versionMinor
+
+    def tzOffset(self):
+        """
+        Returns the timezone offset from GMT.
+
+        :return: int
+        """
+        return self._tzOffset
+
+    def sigfigs(self):
+        """
+        Returns the precision of the timestamps.
+
+        :return: int
+        """
+        return self._sigfigs
+
+    def snaplen(self):
+        """
+        Returns the snapshot length.
+
+        :return: int
+        """
+        return self._snaplen
+
+    def network(self):
+        """
+        Returns the data link type.
+
+        :return: int
+        """
+        return self._network
+
+class PcapRecordHeader:
+    """
+    Represents a PCAP record header.
+    """
+
+    def __init__(self, tsSec, tsFrac, includedLength, originalLength, fileHeader=None, strict=False):
+        #Basic validation for types
+        if tsSec < 0:
+            raise ValueError('ts_sec must not be negative (%s)' % ts_sec)
+        if tsFrac < 0:
+            raise ValueError('ts_frac must not be negative (%s)' % ts_frac)
+        if tsFrac >= (fileHeader.timeResolution() if fileHeader is not None else PCAP_DEFAULT_TIME_RESOLUTION):
+            raise ValueError('ts_frac is too large (%s)' % ts_frac)
+        if includedLength < 0:
+            raise ValueError('included_length must not be negative (%d)' % includedLength)
+        if originalLength < 0:
+            raise ValueError('original_length must not be negative (%d)' % originalLength)
+
+        #Strict validation checks semantics
+        if strict:
+            if originalLength < includedLength:
+                raise ValueError('original_length < included_length (%d < %d)' % (originalLength, includedLength))
+            if fileHeader is not None and fileHeader.snaplen() < includedLength:
+                raise ValueError('file snaplen < included_length (%d < %d)' % (fileHeader.snaplen(), includedLength))
+
+        self._fileHeader = fileHeader
+        self._tsSec = tsSec
+        self._tsFrac = tsFrac
+        self._includedLength = includedLength
+        self._originalLength = originalLength
+
+    def fileHeader(self):
+        """
+        Returns the file header that this record came from (used for time resolution).
+
+        :return: PcapHeader
+        """
+        return self._fileHeader
+
+    def timeResolution(self):
+        """
+        Returns the denominator of the fraction for which the timestamp fraction is the numerator.
+
+        For files with microsecond resolution, this will be 1e6, and for those with nanosecond resolution, it will be 1e9.
+
+        :return: int
+        """
+        return self._fileHeader.timeResolution() if self._fileHeader is not None else PCAP_DEFAULT_TIME_RESOLUTION
+
+    def tsSec(self):
+        """
+        Returns the seconds portion of the timestamp.
+
+        :return: int
+        """
+        return self._tsSec
+
+    def tsFrac(self):
+        """
+        Returns the fraction portion of the timestamp.
+
+        :return: int
+        """
+        return self._tsFrac
+
+    def epochNanos(self):
+        """
+        Returns the timestamp as nanoseconds since epoch.
+
+        :return: int
+        """
+        return self._tsSec * NANOS_PER_SECOND + self._tsFrac * int(NANOS_PER_SECOND / self.timeResolution())
+
+    def epochSecondsFloat(self):
+        """
+        Returns the timestamp as a floating point number of seconds (like time.time might return,
+        suitable for passing to datetime.fromtimestamp).
+
+        :return: float
+        """
+        return self._tsSec + float(self._tsFrac) / self.timeResolution()
+
+    def timestampDatetime(self):
+        """
+        Returns the timestamp a datetime.datetime. Note that this will only have microsecond resolution,
+        regardless of the resolution of the file.
+
+        :return: datetime.datetime
+        """
+        #You might want to be clever and just call self.ts_float(), but don't do it! It's not accurate enough.
+        microseconds = int(self._tsFrac / int(self.timeResolution() / MICROS_PER_SECOND))
+        return datetime.datetime.fromtimestamp(self._tsSec) + datetime.timedelta(microseconds=microseconds)
+
+    def includedLength(self):
+        """
+        Returns the number of bytes included in the file for this packet.
+
+        :return: int
+        """
+        return self._includedLength
+
+    def originalLength(self):
+        """
+        Returns the original number of bytes in the packet.
+
+        :return: int
+        """
+        return self._originalLength
---
+++ Test/test_Format.py
@@ -0,0 +1,71 @@
+
+import unittest
+
+from NanoPcap import Format
+
+class FormatTest(unittest.TestCase):
+
+    def setUp(self):
+        self._initialHeader = (1234, 5678, 9102, -1, 3456, 7890, 4321)
+        self._initialRecordHeader = (1463179445, 10 * 1000 * 1000, 9102, 3456)
+
+    def test_header(self):
+        self.assertEqual(Format.PCAP_HEADER_STRUCT.size, 24)
+
+        initial = self._initialHeader
+        packed = Format.PCAP_HEADER_STRUCT.pack(*initial)
+        unpacked = Format.PCAP_HEADER_STRUCT.unpack(packed)
+        self.assertEqual(unpacked, initial)
+
+        header = Format.PcapHeader(*initial)
+        self.assertEqual(header.magicNumber(), initial[0])
+        self.assertEqual(header.timeResolution(), 1000 * 1000)
+        self.assertEqual(header.versionMajor(), initial[1])
+        self.assertEqual(header.versionMinor(), initial[2])
+        self.assertEqual(header.tzOffset(), initial[3])
+        self.assertEqual(header.sigfigs(), initial[4])
+        self.assertEqual(header.snaplen(), initial[5])
+        self.assertEqual(header.network(), initial[6])
+        self.assertFalse(header.isMagicValid())
+
+    def test_headerMagic(self):
+        correctMagicInitial = list(self._initialHeader)
+        correctMagicInitial[0] = Format.PCAP_MAGIC_NUMBER
+        correctHeader = Format.PcapHeader(*correctMagicInitial)
+        self.assertEqual(correctHeader.magicNumber(), Format.PCAP_MAGIC_NUMBER)
+        self.assertEqual(correctHeader.timeResolution(), 1000 * 1000)
+        self.assertTrue(correctHeader.isMagicValid())
+
+    def test_headerMagicNs(self):
+        correctMagicInitial = list(self._initialHeader)
+        correctMagicInitial[0] = Format.PCAP_NS_MAGIC_NUMBER
+        correctHeaderNs = Format.PcapHeader(*correctMagicInitial)
+        self.assertEqual(correctHeaderNs.magicNumber(), Format.PCAP_NS_MAGIC_NUMBER)
+        self.assertEqual(correctHeaderNs.timeResolution(), 1000 * 1000 * 1000)
+        self.assertTrue(correctHeaderNs.isMagicValid())
+
+    def testRecord(self):
+        self.assertEqual(Format.PCAP_RECORD_HEADER_STRUCT.size, 16)
+
+        initial = self._initialRecordHeader
+        packed = Format.PCAP_RECORD_HEADER_STRUCT.pack(*initial)
+        unpacked = Format.PCAP_RECORD_HEADER_STRUCT.unpack(packed)
+        self.assertEqual(unpacked, initial)
+
+        recordHeader = Format.PcapRecordHeader(*initial)
+        self.assertEqual(recordHeader.tsSec(), initial[0])
+        self.assertEqual(recordHeader.tsFrac(), initial[1])
+        self.assertEqual(recordHeader.epochNanos(), initial[0] * 1000 * 1000 * 1000 + initial[1])
+        self.assertEqual(recordHeader.epochSecondsFloat(), 1463179445.010)
+
+        d = recordHeader.timestampDatetime()
+        self.assertEqual(d.year, 2016)
+        self.assertEqual(d.month, 5)
+        self.assertEqual(d.day, 13)
+        self.assertEqual(d.hour, 17)
+        self.assertEqual(d.minute, 44)
+        self.assertEqual(d.second, 5)
+        self.assertEqual(d.microsecond, 10 * 1000)
+
+        self.assertEqual(recordHeader.includedLength(), initial[2])
+        self.assertEqual(recordHeader.originalLength(), initial[3])
