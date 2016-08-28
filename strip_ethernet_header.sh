9c50e477b0635e1eee2318fde0db95c8d9fd9e39
Ian Hutchinson <ihutchinson@agalmicventures.com>
2016-08-28 15:51:53 -0500

    Convenience script for stripping ethernet headers

---
+++ strip_ethernet_header.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]
+then
+	echo 'Usage: ./strip_ethernet_header.sh <INPUT> <OUTPUT>'
+	exit 2
+fi
+
+set -e
+set -x
+
+#Strip an Ethernet header
+NanoPcap/Tools/PcapFilter.py --required-link-type 1 --link-type 228 -o 14 -x 4 $1 $2
