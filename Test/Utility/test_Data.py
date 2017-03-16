b300f31e8a94454ec325da9b2f509a03e7f5f036
Ian Hutchinson <ihutchinson@agalmicventures.com>
2017-03-15 22:17:20 -0500

    Enhance unit tests

--- Test/Utility/test_Data.py
+++ Test/Utility/test_Data.py
@@ -9,4 +9,5 @@ class DataTest(unittest.TestCase):
         self.assertEqual(Data.randomizeBytes(b'', 0.5), b'')
 
     def test_randomizeBytes_trivial(self):
+        self.assertNotEqual(Data.randomizeBytes(b'1234567890', 0.0), b'')
         self.assertNotEqual(Data.randomizeBytes(b'1234567890', 1.0), b'1234567890')
--- Test/test_Parser.py
+++ Test/test_Parser.py
@@ -88,3 +88,19 @@ class ParserTest(unittest.TestCase):
         self.assertEqual(listener.header().network(), 1)
 
         self.assertEqual(listener.recordHeaders(), [])
+
+    def test_parse(self):
+        listener = Listener.PcapRecordingListener()
+        Parser.parseFile(os.path.join(testDataPath, 'SSH_L3.pcap'), listener)
+
+        self.assertTrue(listener.header() is not None)
+        self.assertTrue(listener.header().isMagicValid())
+        self.assertEqual(listener.header().timeResolution(), 1000 * 1000)
+        self.assertEqual(listener.header().versionMajor(), 2)
+        self.assertEqual(listener.header().versionMinor(), 4)
+        self.assertEqual(listener.header().tzOffset(), 0)
+        self.assertEqual(listener.header().sigfigs(), 0)
+        self.assertEqual(listener.header().snaplen(), 0xFFFF)
+        self.assertEqual(listener.header().network(), 228)
+
+        self.assertEqual(len(listener.recordHeaders()), 21)
